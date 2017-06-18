a165f2f881f476554de2352a21d90fb94bcbd9b4
nick bouldien <nick.bouldien@gmail.com>
2017-06-17 18:42:59 -0700

    practice with react lifecycle events

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import ReactDOM from 'react-dom';
 
 // const App = () => <h1>Hello, World</h1>
 
@@ -8,28 +8,74 @@ class App extends React.Component {
     super(props)
     this.state = {
       txt: 'this is the state txt',
-      cat: 0
+      cat: 0,
+      currentEvent: '---',
+      val: 0
     }
+    this.updateTextArea = this.updateTextArea.bind(this);
+    this.updateVal = this.updateVal.bind(this);
   }
 
+  componentWillMount(){
+    console.log('mounting');
+  }
+
+  componentDidMount(){
+    console.log('did mount');
+  }
+
+  componentWillUnmount(){
+    console.log(' unmount');
+  }
+
+  updateTextArea(e){
+    this.setState({
+      currentEvent: e.type
+    })
+  }
   update (e) {
     this.setState({
       txt: e.target.value
     })
   }
 
+  updateVal(e){
+    this.setState({
+      val: this.state.val + 1
+    })
+  }
+
   render(){
-    let txt = this.props.txt;
+    // let txt = this.props.txt;
+    console.log('render');
+
     return(
       <div>
+
         {/* <h1>Hello, World</h1> */}
         {/* <p>{txt}</p> */}
-        <input
-          type="text"
-          onChange={this.update.bind(this)}\
-        />
+        <Title text="Welcome to React" />
+        <Button>React</Button>
+        <br />
+        <Widget update={this.update.bind(this)}/>
         <p>state txt: {this.state.txt}</p>
         <p>cat: {this.state.cat}</p>
+
+        {/* <p>{this.state.currentEvent}</p>
+        <textarea
+          onKeyPress={this.updateTextArea}
+          onCopy={this.updateTextArea}
+          onDrag={this.updateTextArea}
+          onPaste={this.updateTextArea}
+          onFocus={this.updateTextArea}
+          onBlur={this.updateTextArea}
+
+          rows="10"
+          cols="30"
+        /> */}
+
+        <button onClick={this.updateVal}>{this.state.val}</button>
+
 
       </div>
     )
@@ -45,17 +91,53 @@ App.defaultProps = {
   txt: "this is the default text"
 }
 
+class Wrapper extends React.Component {
+  mount(){
+    ReactDOM.render(<App />, document.getElementById('a'))
+  }
+  unmount(){
+    ReactDOM.unmountComponentAtNode( document.getElementById('a'))
+
+  }
+  render(){
+    return(
+      <div>
+        <button onClick={this.mount.bind(this)}>Mount</button>
+        <hr />
+        <button onClick={this.unmount.bind(this)}>Unmount</button>
+        <div id="a" ></div>
+      </div>
+    )
+  }
+}
 
 
 
+const Button = (props) => (
+  <button>{props.children}</button>
+)
 
+const Widget = (props) => (
+  <input
+    type="text"
+    onChange={props.update}
+  />
+)
 
+const Title = (props) => (
+  <h1>Title: {props.text}</h1>
+)
 
+Title.propTypes = {
+  text(props, propName, component){
+    if(!(propName in props)){
+      return new Error(`missing ${propName}`)
+    }
+    if(props[propName].length < 6 ){
+      return new Error(`${propName} was too short`)
+    }
+  }
+}
 
-
-
-
-
-
-
-export default App;
+export default Wrapper;
+// export default App;
